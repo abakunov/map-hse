2c83ec37af63bf94027f60d4e81a4ca5361336a7
Kirill Abakunov <abakunov@MacBook-Pro-Kirill-3.local>
2023-04-15 18:52:28 +0300

    Swagger added

--- map/urls.py
+++ map/urls.py
@@ -14,12 +14,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import path,include
+from django.urls import re_path
 from api.endpoints import api_urls
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Map API",
+      default_version='v1',
+      description="Backend API for Map HSE testing",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="testing@api.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     re_path(r'^api/', include(api_urls)),
 ]
